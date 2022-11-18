a5144678dca6e290f6d76be6c8d2c8b9df0f1fff
Kevin Blakime <blakime.kevin@gmail.com>
2022-11-18 09:40:40 +0100

    Exo2_temp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  title = Faker::Movie.title
+  overview = Faker::Movie.quote
+  poster_url = 'https://www.nowtv.now.com/wp-content/uploads/2020/01/Spider-Man-No-Way-Home-Extended-Version-mobile.jpg'
+  rating = rand(1..10)
+  movie = Movie.new(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  movie.save
+end
